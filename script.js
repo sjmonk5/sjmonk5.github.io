77f77d01962d16e0dabfafe92494211faa01e09b
sjmonk5 <sjpoliseo@gmail.com>
2016-02-28 14:55:20 -0500

    A lot of changes

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = function(e){
+
+
+function turtleFact() {
+	alert("Some turtles can live longer than humans");
+}
+
+//turtleFact();
+
+function temptGang(gangName) {
+	alert(gangName + " Come out and play!")
+}
+
+function addNumbers(num1, num2) {
+	alert(num1+num2)
+}
+
+//addNumbers(10, 5)
+
+function square(number) {
+	return number * number;
+}
+
+function displaySquare(squaredNumber) {
+	console.log("Your number is " + squaredNumber)
+}
+
+//displaySquare(square(3));
+
+var pageNode = document.getElementsByTagName('body')[0];
+pageNode.innerHTML = '<h1>Oh Noes!</h1> <p>I just changed the whole page!</p>'
+
+pageNode.innerHTML += '...just adding this bit at the end of the page.';
+
+var title = document.getElementById('open-title');
+title.innerHTML = "Thanks for coming to class"
+
+}
